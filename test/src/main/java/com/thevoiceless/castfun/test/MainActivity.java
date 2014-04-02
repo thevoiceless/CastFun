b211a5eef418585f3accb8acef67ec69c80eee1e
Riley Moses <victi.resurgo@gmail.com>
2014-04-02 14:35:38 -0600

    Check if Play services is available

--- test/src/main/java/com/thevoiceless/castfun/test/MainActivity.java
+++ test/src/main/java/com/thevoiceless/castfun/test/MainActivity.java
@@ -4,14 +4,29 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 public class MainActivity extends Activity {
+
+    private TextView message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        message = ((TextView) findViewById(R.id.message));
+
+        final int services = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (services == ConnectionResult.SUCCESS) {
+            message.setText(R.string.play_services_available);
+        } else {
+            message.setText(R.string.play_services_unavailable);
+        }
     }
 
 
